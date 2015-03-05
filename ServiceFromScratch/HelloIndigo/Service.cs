49a4c9a0ff0f71000678efabf53c96faf6893b42
dana.nurse <dana.nurse@outlook.com>
2015-03-05 20:41:37 +0000

    Initial Commit Add Service Contract and initial implementation

---
+++ ServiceFromScratch/HelloIndigo/Service.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace HelloIndigo
+{
+    public class HelloIndigoService :IHelloIndigoService
+    {
+        public string HelloIndigo()
+        {
+            return "Hello Indigo";
+        }
+    }
+
+    [ServiceContract]
+    public interface IHelloIndigoService
+    {
+        [OperationContract]
+        string HelloIndigo();
+    }
+}
