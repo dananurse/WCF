8f51979502c22f75f21e16b0293df2a057f7e71f
dana.nurse <dana.nurse@outlook.com>
2015-03-05 20:49:50 +0000

    Create Console Application to act as a service host

---
+++ ServiceFromScratch/Host/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(HelloIndigo.HelloIndigoService),
+                new Uri("http://localhost:8000/HelloIndigo")))
+            {
+                host.AddServiceEndpoint(typeof (HelloIndigo.IHelloIndigoService),
+                    new BasicHttpBinding(), "HelloIndigoService");
+
+                host.Open();
+
+                Console.WriteLine("Host Running\r\nPress <ENTER> to terminate.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
